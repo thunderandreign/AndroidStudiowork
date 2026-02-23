80da1832c8f918bb1ba11d8d84351557ce1b9467
Khalayi <khalayipatienceritah@gmail.com>
2026-02-23 12:38:10 +0000

    wraper files

--- MyAndroidApp/app/src/main/java/com/example/myapp/MainActivity.java
+++ MyAndroidApp/app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,13 +1,13 @@
-package com.example.myapp;
+// MyAndroidApp/app/src/main/java/com/example/myandroidapp/MainActivity.java
+package com.example.myandroidapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(android.R.layout.simple_list_item_1);
     }
 }
